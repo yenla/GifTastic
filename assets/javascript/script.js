a784979409c54938055605bdd07f40486d553cbc
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-03-11 22:45:18 -0800

    Add more script and css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,37 +1,58 @@
-var topics = ["Surfing", "Skate Boarding", "Snow Boarding", "Skiing"];
+var topics = ["Surfing", "Skate Boarding", "Wakeboarding", "Snow Boarding", "Skiing", "Soccer", "Football", "BMX", "Basketball", "Kayak"];
   
 function displaySport() {
-  var sport = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+
+    var sport = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
       sport + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-  $.ajax({
+    $.ajax({
     url: queryURL,
     method: "GET"
-  }).done(function(response){
+    }).done(function(response) {
 
     console.log(response);
     var results = response.data;
 
-    for (var i = 0; i < results.length; i++) {
-      var sportDiv = $("<div class='sport'>");
+      for (var i = 0; i < results.length; i++) {
+        var sportDiv = $("<div class='sport-class' title='Click on gif to start/stop animation'>");
 
-      var rating = results[i].rating;
+        var img = $("<img>").on("click", function() {
+          var state = $(this).attr("data-state");
 
-      var p = $("<p>").text("Rating: " + rating);
+          if (state === "still") {
+            var changeState = $(this).attr("data-animate");
+            $(this).attr("src", changeState);
+            $(this).attr("data-state", "animate");
+          }
 
-      var sportImage = $("<img>");
-      sportImage.attr("src", results[i].images.fixed_height.url);
+          if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
 
-      sportDiv.prepend(p);
-      sportDiv.prepend(sportImage);
+        });
 
-      $("#sports-div").prepend(sportDiv);
-    }
+        var rating = results[i].rating;
 
-  });
+        var p = $("<p>").text("Rating: " + rating);
+
+        var still = results[i].images.fixed_height_still.url;
+        var animate = results[i].images.fixed_height.url;
+        img.attr("src", still);
+        img.attr("data-still", still);
+        img.attr("data-animate", animate);
+        img.attr("data-state", "still");
+        sportDiv.append(img);
+
+        sportDiv.prepend(p);
+
+        $("#sports-div").append(sportDiv);
+      }
+
+    });
 }
-
+    
 function renderButtons() {
 
   $("#buttons").empty();
@@ -57,6 +78,7 @@ $("#add-sport").on("click", function(event) {
   renderButtons();
 
 });
+
 
 $(document).on("click", ".sport", displaySport);
 
